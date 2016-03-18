af6c97ad7f6b39e87d16baee052458a78382a8f5
wangruwei <wangruwei@hotmail.com>
2016-03-18 11:43:23 +0800

    update demo9

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('sass', function () {
+	return sass(['css/main.scss'], { sourcemap: true})
+    .pipe(sourcemaps.write('maps', {
+      includeContent: false,
+      sourceRoot: 'css'
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function(){
+	gulp.watch('css/**/*.scss', ['sass']);
+});
+
+gulp.task('default',['sass', 'watch']);
+
+livereload = require('livereload');
+server = livereload.createServer();
+server.watch(__dirname);
+console.log('livereload start');
